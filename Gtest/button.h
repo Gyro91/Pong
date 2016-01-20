7b4de25f5736bbe9563dc5143bb560fcb5aef60d
MATTEO <rotundo.matteo@gmail.com>
2016-01-20 18:16:07 +0100

    first commit

---
+++ Gtest/button.h
@@ -0,0 +1,11 @@
+
+class Button{
+
+public:
+
+  virtual ~Button() {}
+  virtual void release() = 0;
+  virtual void push() = 0;
+
+
+};
---
+++ Gtest/googletest-master/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+GTEST_DIR="googletest"
+GMOCK_DIR="googlemock"
+
+g++ -isystem ${GTEST_DIR}/include -I${GTEST_DIR} \
+    -isystem ${GMOCK_DIR}/include -I${GMOCK_DIR} \
+    -pthread -c ${GTEST_DIR}/src/gtest-all.cc
+g++ -isystem ${GTEST_DIR}/include -I${GTEST_DIR} \
+    -isystem ${GMOCK_DIR}/include -I${GMOCK_DIR} \
+    -pthread -c ${GMOCK_DIR}/src/gmock-all.cc
+ar -rv libgmock.a gtest-all.o gmock-all.o
